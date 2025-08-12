e81baddc2db879b79c31ebb4700080cf731d8ce3
rrodriguez-2023342 <rrodriguez-2023342@kinal.edu.gt>
2025-08-11 21:27:06 -0600

    feat: Se subio el mapeo de las clases faltantes

--- Java Web/KinalitosClothes/src/java/com/kinalitosclothes/modelo/Categorias.java
+++ Java Web/KinalitosClothes/src/java/com/kinalitosclothes/modelo/Categorias.java
@@ -1,5 +1,77 @@
 package com.kinalitosclothes.modelo;
 
 public class Categorias {
-    
+    private int codigoCategoria;
+    private String nombreCategoria, descripcionCategoria;
+    private Genero genero;
+    private RangoEdad rangoEdad;
+
+    public enum Genero {
+        Hombre, Mujer, Unisex
+    }
+
+    public enum RangoEdad {
+        Infantil, Juvenil, Adultos
+    }
+
+    public Categorias() {
+    }
+
+    public Categorias(int codigoCategoria, String nombreCategoria, String descripcionCategoria, Genero genero, RangoEdad rangoEdad) {
+        this.codigoCategoria = codigoCategoria;
+        this.nombreCategoria = nombreCategoria;
+        this.descripcionCategoria = descripcionCategoria;
+        this.genero = genero;
+        this.rangoEdad = rangoEdad;
+    }
+
+    public int getCodigoCategoria() {
+        return codigoCategoria;
+    }
+
+    public void setCodigoCategoria(int codigoCategoria) {
+        this.codigoCategoria = codigoCategoria;
+    }
+
+    public String getNombreCategoria() {
+        return nombreCategoria;
+    }
+
+    public void setNombreCategoria(String nombreCategoria) {
+        this.nombreCategoria = nombreCategoria;
+    }
+
+    public String getDescripcionCategoria() {
+        return descripcionCategoria;
+    }
+
+    public void setDescripcionCategoria(String descripcionCategoria) {
+        this.descripcionCategoria = descripcionCategoria;
+    }
+
+    public Genero getGenero() {
+        return genero;
+    }
+
+    public void setGenero(Genero genero) {
+        this.genero = genero;
+    }
+
+    public RangoEdad getRangoEdad() {
+        return rangoEdad;
+    }
+
+    public void setRangoEdad(RangoEdad rangoEdad) {
+        this.rangoEdad = rangoEdad;
+    }
+
+    @Override
+    public String toString() {
+        return "Categorias{" + "Codigo Categoria: " + codigoCategoria
+                + "\nNombre Categoria: " + nombreCategoria
+                + "\nDescripcion Categoria=" + descripcionCategoria
+                + "\nGenero: " + genero
+                + "\nRango Edad: " + rangoEdad + '}';
+    }
+
 }
--- Java Web/KinalitosClothes/src/java/com/kinalitosclothes/modelo/Facturas.java
+++ Java Web/KinalitosClothes/src/java/com/kinalitosclothes/modelo/Facturas.java
@@ -1,5 +1,113 @@
 package com.kinalitosclothes.modelo;
 
+import java.util.Date;
+
 public class Facturas {
-    
+
+    private int codigoFactura;
+    private Date fechaEmision;
+    private Double descuentoAplicado;
+    private Double totalFactura;
+    private EstadoFactura estadoFactura;
+    private FormaEntrega formaEntrega;
+    private int codigoPedido;
+    private int codigoEmpleado;
+
+    public enum EstadoFactura {
+        Emitida, Anulada, Pagada
+    }
+
+    public enum FormaEntrega {
+        Fisica, Electronica
+    }
+
+    public Facturas() {
+    }
+
+    public Facturas(int codigoFactura, Date fechaEmision, Double descuentoAplicado, Double totalFactura, EstadoFactura estadoFactura, FormaEntrega formaEntrega, int codigoPedido, int codigoEmpleado) {
+        this.codigoFactura = codigoFactura;
+        this.fechaEmision = fechaEmision;
+        this.descuentoAplicado = descuentoAplicado;
+        this.totalFactura = totalFactura;
+        this.estadoFactura = estadoFactura;
+        this.formaEntrega = formaEntrega;
+        this.codigoPedido = codigoPedido;
+        this.codigoEmpleado = codigoEmpleado;
+    }
+
+    public int getCodigoFactura() {
+        return codigoFactura;
+    }
+
+    public void setCodigoFactura(int codigoFactura) {
+        this.codigoFactura = codigoFactura;
+    }
+
+    public Date getFechaEmision() {
+        return fechaEmision;
+    }
+
+    public void setFechaEmision(Date fechaEmision) {
+        this.fechaEmision = fechaEmision;
+    }
+
+    public Double getDescuentoAplicado() {
+        return descuentoAplicado;
+    }
+
+    public void setDescuentoAplicado(Double descuentoAplicado) {
+        this.descuentoAplicado = descuentoAplicado;
+    }
+
+    public Double getTotalFactura() {
+        return totalFactura;
+    }
+
+    public void setTotalFactura(Double totalFactura) {
+        this.totalFactura = totalFactura;
+    }
+
+    public EstadoFactura getEstadoFactura() {
+        return estadoFactura;
+    }
+
+    public void setEstadoFactura(EstadoFactura estadoFactura) {
+        this.estadoFactura = estadoFactura;
+    }
+
+    public FormaEntrega getFormaEntrega() {
+        return formaEntrega;
+    }
+
+    public void setFormaEntrega(FormaEntrega formaEntrega) {
+        this.formaEntrega = formaEntrega;
+    }
+
+    public int getCodigoPedido() {
+        return codigoPedido;
+    }
+
+    public void setCodigoPedido(int codigoPedido) {
+        this.codigoPedido = codigoPedido;
+    }
+
+    public int getCodigoEmpleado() {
+        return codigoEmpleado;
+    }
+
+    public void setCodigoEmpleado(int codigoEmpleado) {
+        this.codigoEmpleado = codigoEmpleado;
+    }
+
+    @Override
+    public String toString() {
+        return "Facturas{" + "\nCodigo Factura: " + codigoFactura
+                + "\nFecha Emision: " + fechaEmision
+                + "\nDescuento Aplicado: " + descuentoAplicado
+                + "\nTotal Factura: " + totalFactura
+                + "\nEstado Factura: " + estadoFactura
+                + "\nForma Entrega: " + formaEntrega
+                + "\nCodigo Pedido: " + codigoPedido
+                + "\nCodigo Empleado: " + codigoEmpleado + '}';
+    }
 }
